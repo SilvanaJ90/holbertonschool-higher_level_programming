e6ed57e954daa89e48966e7c29cccd5102282ed8
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-09-21 13:46:56 -0500

    update

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,2 +1,5 @@
 #!/usr/bin/python3
-print("\"Programming is like building a multilingual puzzle")
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str = str[39:66] + str[106:112] + str[0:6]
+print(str)
