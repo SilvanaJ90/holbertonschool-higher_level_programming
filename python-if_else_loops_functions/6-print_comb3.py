1ea4bfc5f1ef0823d8914c6f16f6d658bba423a1
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-09-23 17:50:57 -0500

    update

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+cont = 0
+j = 0
+for i in range(0, 8):
+    cont = cont + 1
+    j = cont
+    for j in range(j, 10):
+        if i != j:
+            print("{}{}".format(i, j), end=", ")
+print("89")
