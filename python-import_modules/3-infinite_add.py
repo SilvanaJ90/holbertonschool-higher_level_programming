285abe8514edd23981e9fefd317cc31223aaabed
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-09-24 20:21:33 -0500

    update

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,1 +1,11 @@
-#!/usr/bin/bash
+#!/usr/bin/python3
+if __name__ == '__main__':
+    import sys
+    fun = sys.argv
+    n = len(fun)
+    suma = 0
+    suma = int(suma)
+    for i in range(1, n):
+        fun[i] = int(fun[i])
+        suma = suma + fun[i]
+    print("{}".format(suma))
--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,1 +1,3 @@
-#!/usr/bin/bash
+ #!/usr/bin/python3
+import imp
+my_module = imp.load_compiled("my_module", "full_path_to_my_module/my_module.pyc")
