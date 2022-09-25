3ed7d282bdc7b9784f04306c715c98f64bca27a6
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-09-25 16:15:02 -0500

    creat-5

--- python-import_modules/5-variable_load.py
+++ python-import_modules/5-variable_load.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-import variable_load_5
+from variable_load_5 import a
 if __name__ == '__main__':
-    new_a = variable_load_5.a
-    print ("{}".format(new_a))
+    print ("{}".format(a))
