55ee5273a7d33e85bd9b4b028e4cf46d8518b598
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-09-24 19:38:02 -0500

    update

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/python3
 if __name__ == '__main__':
     import sys
@@ -6,6 +5,10 @@ if __name__ == '__main__':
     n = len(fun)
     if n == 1:
         print("{} arguments.".format(n - 1))
+    elif n == 2:
+        print("{} argument.".format(n - 1))
+        for i in range(1, n):
+            print("{}: {}".format(i, fun[i]))
     else:
         print("{} arguments:".format(n - 1))
         for i in range(1, n):
