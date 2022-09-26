db12fd59652fab274e7257abb60e197ee9100ae6
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-09-25 20:40:47 -0500

    update

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     if idx < 0:
-        return my_list.copy()
+        return my_list
     elif idx > len(my_list) - 1:
-        return my_list.copy()
+        return my_list
     else:
         list = my_list.copy()
         my_list[idx] = element
