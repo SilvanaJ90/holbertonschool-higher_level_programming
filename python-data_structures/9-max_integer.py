3ebb1e566e4c46c85059309528aa22dd1c163369
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-09-26 17:02:43 -0500

    update

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -3,7 +3,7 @@ def max_integer(my_list=[]):
     large = my_list[0]
     large_int = int(large)
     for i in my_list:
-        if large == 0:
+        if len(my_list) == 0:
             return None
         elif i > large_int:
             large_int = i
