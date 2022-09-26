698fa050b3a8cbebabdca495e600ee1c8e186f9c
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-09-26 16:26:50 -0500

    update

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     res = int(len(sentence))
-    print("Length: {} - First character: {}".format(res, sentence[:1]))
+    return res, sentence[:1]
