e500bda14eb81a89e668866632e63afbb1f75ac6
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-09-26 15:03:55 -0500

    create

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -5,4 +5,4 @@ def add_tuple(tuple_a=(), tuple_b=()):
         tuple_c.append(tuple_a[i])
     for i in range(len(tuple_b)):
         tuple_c[i] = tuple_c[i] + tuple_b[i]
-    print(tuple_c)
+    return tuple_c
