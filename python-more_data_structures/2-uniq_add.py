09b7eb44bb66b1cd0a4221ed4a9270755fd1b927
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-09-27 17:02:38 -0500

    update

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    for i in my_list:
-        uniq = set(my_list)
-        if i <= 0:
-            return 0
-    return sum(uniq)
+    return sum(set(my_list))
