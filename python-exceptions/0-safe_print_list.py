c2f4b95085f6adbae428b91db3fa3f800e7a4bcb
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-09-29 16:37:14 -0500

    create-0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    try:
-        for x in my_list:
-            return x
-    except ValueError:
-        print("{} is not list".format(x))
+    count = 0
+    for i in my_list[:x]:
+        try:
+            print(i, end="")
+            count += 1
+        except IndexError:
+            print()
+    print()
+    return count
