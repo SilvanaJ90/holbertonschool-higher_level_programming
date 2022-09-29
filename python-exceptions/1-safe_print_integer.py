72d4de3673697bee65c8df21731188637ee21522
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-09-29 16:45:47 -0500

    create-1

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+def safe_print_integer(value):
+    value = 0
+    try:
+        print("{:d}".format(value),end="")
+    except ValueError:
+        if not print:
+            return False
+        else:
+            True
