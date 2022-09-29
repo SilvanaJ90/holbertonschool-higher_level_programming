dcc84fb2939a7b621b1911a167bc3be885b92782
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-09-29 09:53:29 -0500

    create-directory

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,1 +1,6 @@
 #!/usr/bin/python3
+def roman_to_int(roman_string):
+    roman_string = {"X": 10, "VII": 10, "IX": 10, "LXXXVII": 10, "DCCVII": 10}
+    for key, value in roman_string.items():
+        while key != None:
+            return roman_string.replace(key, value)
