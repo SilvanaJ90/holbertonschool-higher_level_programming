6281c88b375be7fa8cf92a4a21f53c3848c57be5
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-09-29 18:54:55 -0500

    update

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        div = a / b
+        print("{}".format(div))
+    except ZeroDivisionError:
+        print("None")
+    finally:
+        print("{}".format(div))
+        
