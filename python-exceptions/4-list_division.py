c769c148937f984ddc46716630075ddc08a2098d
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-09-29 22:48:52 -0500

    create 5

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    try:
+        for  in list_length[0]:
+        resul = list_length(my_list_1) / list_length(my_list_2)
+    except ZeroDivisionError:
+        print("Inside result: None")
+    finally:
+        if my_list_2 == 0:
+            return None
+        else:
+            print("Inside result: {}".format(resul))
+            return resulf
--- python-exceptions/5-raise_exception.py
+++ python-exceptions/5-raise_exception.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+from weakref import finalize
+
+
+def raise_exception():
+    try:
+        raise KeyboardInterrupt
+    finally:
+        print('Exception raised')
