318d4819251f4d8d5abbfb0f27ea1502791f32a0
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-09-30 15:22:34 -0500

    create2

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+""" Class Square """
+
+class Square:
+    """ create Square size private"""
+    try:
+        def __init__(self, size=0):
+            self.__size = size
+    
+    except TypeError:
+        print("size must be an integer")
+    except ValueError:
+        print("size must be >= 0")
