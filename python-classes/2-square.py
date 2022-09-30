127eb50929bbd096cc7fdfe37fe7173d1776b82b
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-09-30 17:36:57 -0500

    create2

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -3,11 +3,10 @@
 
 class Square:
     """ create Square size private"""
-    try:
-        def __init__(self, size=0):
-            self.__size = size
-    
-    except TypeError:
-        print("size must be an integer")
-    except ValueError:
-        print("size must be >= 0")
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >0= ")
+        self.__size = size
+        
