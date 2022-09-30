c67aa2bc682cff23ecadde0ba25f23b6c7e14e7d
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-09-30 06:19:30 -0500

    create-5

--- python-exceptions/5-raise_exception.py
+++ python-exceptions/5-raise_exception.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
-from weakref import finalize
-
-
 def raise_exception():
-    try:
-        raise KeyboardInterrupt
-    finally:
-        print('Exception raised')
+    raise RuntimeError from exec
