f71f983b6b5c68c50669df88223700b81a1b402d
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-09-30 19:23:06 -0500

    create4

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+
+""" Class Square """
+
+
+class Square:
+    """ create Square size private"""
+    def __init__(self, size=0):
+        self.__size = size
+
+    def area(self):
+        return (self.__size) * (self.__size)
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
