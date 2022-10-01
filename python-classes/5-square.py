f06e6d177d3ee8b6c97a5b9ef55fd7f00ff1b7f2
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-09-30 19:39:03 -0500

    create-5

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -1,1 +1,33 @@
 #!/usr/bin/python3
+
+""" Class Square """
+
+
+class Square:
+    """ create Square size private"""
+    def __init__(self, size=0):
+        self.__size = size
+
+    def area(self):
+        area = (self.__size) * (self.__size)
+        return area
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def my_print(self):
+        if self.__size == 0:
+            print("")
+        for i in range(self.__size):
+            for j in range(self.__size):
+                print("#", end="")
+            print("")
