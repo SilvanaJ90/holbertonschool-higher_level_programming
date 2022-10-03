89603924058b7be4ee5fec9e7a466b6df09ed124
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-10-03 13:42:46 -0500

    create-0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
-""" create function add a and b"""
+"""
+This is the "0-add_integer" module.
+
+The 0-add_integer module is function that adds 2 integers, add_integer(a, b).
+"""
+
 
 def add_integer(a, b=98):
-    """ function add """
-    
+    """ Returns an integer: the addition of a and b 
+
+    """
     if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
     if type(b) is not int and type(b) is not float:
@@ -12,4 +18,4 @@ def add_integer(a, b=98):
         a = int(a)
     if type(b) is float:
         b = int(b)
-    return (a + b)
+    return a + b
