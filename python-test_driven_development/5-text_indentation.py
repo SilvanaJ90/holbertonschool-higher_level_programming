12c5939db0f00e898364fc5b00949058c2ab0d7a
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-10-04 14:46:10 -0500

    update

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,8 +1,25 @@
 #!/usr/bin/python3
+"""
+This is the "5-text_indentation.py" module.
+
+The function that print 2 new lines after each of these characters.
+"""
+
+
 def text_indentation(text):
-    for i in len(text):
-        if type(text[i]) is not str:
-            raise TypeError("text must be a string")
-        if text[i] == '?' or text[i] == 'and' or text[i] == '.':
-            print("{}".format(text[i]), end="\n")
-    print()
+    """ function that prints a text with 2 new lines """
+
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+    after_new_line = False
+    for c in text:
+        if after_new_line:
+            if c == " ":
+                continue
+            after_new_line = False
+        if c == '.' or c == '?' or c == ':':
+            print(c)
+            print("")
+            after_new_line = True
+        else:
+            print(c, end="")
