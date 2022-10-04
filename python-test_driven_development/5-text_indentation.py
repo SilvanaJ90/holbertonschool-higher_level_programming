217f70a9d131780408536cd2a1a8918b98b3ea7f
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-10-04 13:40:38 -0500

    update

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+def text_indentation(text):
+    for i in len(text):
+        if type(text[i]) is not str:
+            raise TypeError("text must be a string")
+        if text[i] == '?' or text[i] == 'and' or text[i] == '.':
+            print("{}".format(text[i]), end="\n")
+    print()
