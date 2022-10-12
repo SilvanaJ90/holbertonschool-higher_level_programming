af71910426b5fd3838392f9ae7d2d88019e09e58
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-10-12 13:48:23 -0500

    update

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -6,6 +6,7 @@ def inherits_from(obj, a_class):
     """ if the object is an instance of a class that inherited"""
     if (issubclass(int, a_class)):
         return True
+    if (issubclass(object, a_class)):
+        return True
     else:
-        issubclass(a_class, object)
         return False
