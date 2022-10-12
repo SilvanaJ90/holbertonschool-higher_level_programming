847134dc0cccce1af444e38f0d6e0aaa9633d461
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-10-12 13:27:31 -0500

    update

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Doc """
+
+
+from mimetypes import init
+
+
+def inherits_from(obj, a_class):
+    """ if the object is an instance of a class that inherited"""
+    if issubclass(a_class, object):
+        return True
+    if issubclass(obj, object):
+        return True
+    else:
+        return False
