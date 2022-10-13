f985fe55289f2ad78738f8263310834b41d925a9
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-10-12 23:25:12 -0500

    update

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -32,9 +32,9 @@ class Rectangle(BaseGeometry):
 class Square(Rectangle):
     """ Doc """
     def __init__(self, size):
-        super().__init__(size, size)
-        super().integer_validator("size", size)
+        self.integer_validator("size", size)
         self.__size = size
+        super().__init__(size, size)
 
     def area(self):
         return self.__size * self.__size
