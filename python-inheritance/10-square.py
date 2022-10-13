3ad54b9c63555e54d5ec7c4c200b38a1ad05925d
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-10-12 23:23:38 -0500

    update

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -8,9 +8,9 @@ Rectangle = __import__('9-rectangle').Rectangle
 class Square(Rectangle):
     """ Doc """
     def __init__(self, size):
-        super().__init__(size, size)
-        super().integer_validator("size", size)
+        self.integer_validator("size", size)
         self.__size = size
+        super().__init__(size, size)
 
     def area(self):
         return self.__size * self.__size
