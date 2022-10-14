fcf6f01dc4e4621ebe699df90c26b53bde0660bc
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-10-14 14:12:15 -0500

    update

--- python-input_output/1-main.py
+++ python-input_output/1-main.py
@@ -2,6 +2,6 @@
 write_file = __import__('1-write_file').write_file
 
 filename = "o_file"
-text = "Holberton School is so cool!\n"
+text = "H"
 nb_characters = write_file(filename, text)
 print(nb_characters)
--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -7,7 +7,4 @@ Doc
 def write_file(filename="", text=""):
     """ Doc """
     with open("my_first_file.txt", mode="w", encoding="utf-8") as filename:
-        filename.write(text)
-
-    return(len(text))
-   
+        return filename.write(text)
