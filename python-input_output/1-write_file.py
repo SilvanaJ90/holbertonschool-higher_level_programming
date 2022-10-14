190cfd4e969404127e16cf990ef8877369cfe4a7
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-10-14 13:52:17 -0500

    update-1

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -6,7 +6,6 @@ Doc
 
 def write_file(filename="", text=""):
     """ Doc """
-    text = "This School is so cool!\n"
     with open("my_first_file.txt", mode="w", encoding="utf-8") as filename:
         filename.write(text)
     print(len(text))
