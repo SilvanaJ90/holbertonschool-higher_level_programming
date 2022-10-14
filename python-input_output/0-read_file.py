fdf9440f0c3bbdb17176acd677c07732297e7e83
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-10-14 13:03:19 -0500

    update

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -6,5 +6,5 @@ Doc
 
 def read_file(filename=""):
     """ Doc """
-    with open("my_file_0.txt", encoding="utf-8") as filename:
-        print(filename.read())
+    with open("my_file_0.txt", encoding="utf-8") as f:
+        print(f.read(), end="")
