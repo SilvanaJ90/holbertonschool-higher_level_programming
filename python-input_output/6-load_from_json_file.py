81df7d6d50a05d02d37be2601061ffc4cdb6514c
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-10-14 19:48:56 -0500

    update

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -2,11 +2,11 @@
 """
 Doc
 """
-from distutils.file_util import move_file
+
 import json
 
 
 def load_from_json_file(filename):
     """ Write a function that creates an Object from a “JSON file"""
-    with open(filename, 'w') as outfile:
-        json.load(filename, outfile)
+    with open(filename, "r", encoding="utf-8") as f:
+        return json.load(f)
