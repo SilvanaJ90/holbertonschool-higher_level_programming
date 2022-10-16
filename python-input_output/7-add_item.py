6dca515a54dd2236616216493720fdf98b4b4fc2
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-10-15 20:04:06 -0500

    update

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -2,15 +2,18 @@
 """ Write a script that adds all arguments to a Python list """
 
 
-import sys
+from sys import argv
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-args = sys.argv
 filename = "add_item.json"
-with open(filename, 'a+', encoding="utf-8") as f:
+try:
+    my_list = load_from_json_file(filename)
+except:
     my_list = []
-    my_list.extend(args[1:])
-    save_to_json_file(my_list, filename)
-    load_from_json_file(filename)
+
+for arg in argv[1:]:
+    my_list.append(argv)
+save_to_json_file(my_list, filename)
+  
