d7b1bda195851605865cc27532d68033d5e92444
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-10-17 12:00:44 -0500

    update

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -8,11 +8,11 @@ def pascal_triangle(n):
     """ Function return a list of lists of integers"""
     if n <= 0:
         return []
-    triangle = []
-    row = []
-    p_row = []
-    for i in range(0, n + 1):
-        row = [j > 0 and j < i - 1 and i > 2 and p_row[j-1] + p_row[j] or 1 for j in range(0, i)]
-        p_row = row
-        triangle += [row]
-    return triangle[1:]
+    triangule = [[1]]
+    for x in range(1, n):
+        m = [1]
+        for y in range(1, x):
+            m.append(triangule[x-1][y-1] + triangule[x-1][y])
+        m.append(1)
+        triangule.append(m)
+    return triangule
