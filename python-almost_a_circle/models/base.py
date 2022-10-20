56c7170ac81824775b10f5dd48c3912fd4d9ad05
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-10-20 08:06:09 -0500

    update-json-dumps

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -2,6 +2,7 @@
 """
 class base
 """
+import json
 
 
 class Base:
@@ -15,3 +16,9 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """ return JSON string of list dictionaries """
+        if list_dictionaries is None or len(list_dictionaries) <= 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
