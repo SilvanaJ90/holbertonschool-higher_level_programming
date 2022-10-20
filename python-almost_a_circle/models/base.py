97d41c9cdf6e96e6e844939523d8bfef03e57191
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-10-20 08:06:54 -0500

    update-json-dumps

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -19,6 +19,6 @@ class Base:
 
     def to_json_string(list_dictionaries):
         """ return JSON string of list dictionaries """
-        if list_dictionaries is None or len(list_dictionaries) <= 0:
+        if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         return json.dumps(list_dictionaries)
