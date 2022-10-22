85495bc0fa2b348f5c49e5ba1491aa5b9276b5a1
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-10-22 15:23:11 -0500

    update

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -5,15 +5,22 @@ import unittest
 from models.base import Base
 
 
-class BaseTest(unittest.TestCase):
+class TestBase(unittest.TestCase):
     """ doc """
     def test_base(self):
+    # Test of Base() for assigning automatically an ID
         b1 = Base()
         self.assertEquals(b1.id, 1)
-        b2 = Base()
+    # Test of Base() for assigning automatically an ID + 1 of the previous
+    def tes_base_id(self):
+        b2 = Base.__nb_objects
         self.assertEquals(b2.id, 2)
+    # Test of Base(89) saving the ID passed
+    def test_base_saving(self):
         b2 = Base(89)
         self.assertEquals(b2.id, 89)
+
+
 
 if __name__ == '__main__':
     unittest.main()
