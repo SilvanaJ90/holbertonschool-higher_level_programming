8695ee56bbf38caa89561a913cc128abdaeaef13
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-10-23 23:24:43 -0500

    update

--- python-almost_a_circle/tests/test_square.py
+++ python-almost_a_circle/tests/test_square.py
@@ -18,8 +18,35 @@ class TestSquare(unittest.TestCase):
     
     """ Test of Square(1) exists """
     def test_square(self):
-        r = Square(1)
-        self.assertEqual(r.width, 1)
+        s = Square(1)
+        self.assertEqual(s.width, 1)
+
+
+    """ Test of Square(1, 2) exists """
+    def test_square_w(self):
+        s = Square(1, 2)
+        self.assertEqual(s.width, 1)
+        self.assertEqual(s.x, 2)
+
+
+    """ Test of Square(1, 2, 3) exists """
+
+    """ Test of Square("1") exists """
+
+    """ Test of Square(1, "2") exists """
+
+    """ Test of Square(1, 2, "3") exists """
+
+    """ Test of Square(1, 2, 3, 4) exists """
+
+    """ Test of Square(-1) exists """
+
+    """ Test of Square(1, -2) exists """
+
+    """ Test of Square(1, 2, -3) exists """
+    
+    """ Test of Square(0) exists """
+
 
 
 if __name__ == '__main__':
