2fc8c5552a8aed509780e3faed7f663453a6aaaa
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-10-24 08:08:01 -0500

    update

--- 0x12-python-almost_a_circle/models/base.py
+++ 0x12-python-almost_a_circle/models/base.py
@@ -53,11 +53,13 @@ class Base:
     def load_from_file(cls):
         """ Return list instances """
         filename = cls.__name__+".json"
-        if filename is None:
-            return []
-        else:
+
+        try:
             with open(filename, encoding="utf-8") as f:
                 resul = f.read()
                 json_resul = cls.from_json_string(resul)
                 newList = [cls.create(**obj) for obj in json_resul]
                 return newList
+        except:
+            pass
+            return []
