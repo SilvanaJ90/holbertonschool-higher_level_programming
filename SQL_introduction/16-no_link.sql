d4722781e20dad7b7a95e1b9b91471e86c6c85fb
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-11-10 15:07:59 -0500

    update

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- script that lists all records of the table second_table of the database 
-mysql> SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
+mysql> SELECT score, name FROM second_table WHERE name != '' OR IS NOT NULL ORDER BY score DESC;
