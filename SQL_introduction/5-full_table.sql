ea378d65687a8c339ceefc7e4291451b63d172e9
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-11-10 13:01:12 -0500

    update

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,2 +1,2 @@
--- Script that prints the full description of the table first_table from the database hbtn_0c_0 in your MySQL server.
+-- Script describe table
 SHOW COLUMNS FROM hbtn_0c_0.first_table;
