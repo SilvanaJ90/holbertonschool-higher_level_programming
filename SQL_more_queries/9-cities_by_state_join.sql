34a20be0407457eef907dee2ece46c828eb92038
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-11-14 18:18:15 -0500

    update

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,7 +1,2 @@
 -- Write a script that lists all cities contained in the database hbtn_0d_usa.
- Cada registro debe mostrar: ciudades.id - ciudades.nombre - estados.nombre
-    Los resultados deben estar ordenados de forma ascendente por cities.id
-    Sólo se puede utilizar una sentencia SELECT
-    El nombre de la base de datos se pasará como argumento del comando mysql
-
 SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states ORDER BY cities.id ASC;
