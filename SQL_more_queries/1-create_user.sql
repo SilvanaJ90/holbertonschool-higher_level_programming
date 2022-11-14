85e42c6e83a8cb868a2bf90e9f802de5cb93fafe
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-11-14 16:13:40 -0500

    update

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,2 +1,3 @@
 -- Write a script that creates the MySQL server user user_0d_1. 
 CREATE USER 'user_0d_1'@'localhost' IDENTIFIED WIDTH authentication_plugin BY 'user_0d_1_pwd';
+GRANT PRIVILEGE ON database.table TO 'user_0d_1'@'localhost';
