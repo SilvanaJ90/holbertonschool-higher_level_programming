ccf375533e81ff6a9ca3da8162222b9fb2a1e65c
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-11-15 15:24:45 -0500

    update

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -6,20 +6,26 @@ from sys import argv
 
 
 def mysqlconnect():
-    """ Function for connecting to MySQL database""" 
-    
-    db = MySQLdb.connect(
-        host="localhost",
-        user=argv[1],
-        password='',
-        database=argv[3],
-        port=3306
-        )
-               
+    """ Function for connecting to MySQL database"""
+
+    try:
+        db = MySQLdb.connect(
+            host="localhost",
+            user=argv[1],
+            password='',
+            database=argv[3],
+            port=3306
+            )
+    except:
+        print("Can't connect to database")
+        return 0
+    """If Connection Is Successful"""
+    print("Connected")
+
     cursor = db.cursor()
 
     sql = "SELECT * FROM states ORDER BY states.id ASC"
- 
+
     cursor.execute(sql)
 
     rows = cursor.fetchall()
@@ -29,6 +35,5 @@ def mysqlconnect():
     db.close()
 
 
-
-if __name__=="__main__":
+if __name__ == "__main__":
     mysqlconnect()
