2755e6fb286c25564af3a5bde957cb34e3fb6138
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-11-15 14:16:34 -0500

    update

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -2,15 +2,16 @@
 import MySQLdb
 """ Write a script that lists all states from the database hbtn_0e_0_usa:  """
  
-"""Open database connection """ 
+
 db = MySQLdb.connect(host="localhost",user="root",password='',database="hbtn_0e_0_usa",port=3306)
+"""Open database connection """ 
  
 cursor = db.cursor()
- 
-"""Create table as per requirement"""
+
 sql = "SELECT * FROM states ORDER BY states.id ASC"
+"""Create table as per requirement"""
  
 cursor.execute(sql) #table created
- 
-"""disconnect from server"""
+
 db.close()
+"""disconnect from server"""
