033519aa6b9f8f1ad86b86c07a741040864a80f0
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-11-15 14:22:12 -0500

    update

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,17 +1,29 @@
 #!/usr/bin/python3
 import MySQLdb
 """ Write a script that lists all states from the database hbtn_0e_0_usa:  """
- 
 
-db = MySQLdb.connect(host="localhost",user="root",password='',database="hbtn_0e_0_usa",port=3306)
-"""Open database connection """ 
- 
-cursor = db.cursor()
+""" Function for connecting to MySQL database"""
+def mysqlconnect():
 
-sql = "SELECT * FROM states ORDER BY states.id ASC"
-"""Create table as per requirement"""
- 
-cursor.execute(sql) #table created
+    try:
 
-db.close()
-"""disconnect from server"""
+        db = MySQLdb.connect(host="localhost",user="root",password='',database="hbtn_0e_0_usa",port=3306)
+        """Open database connection """ 
+    except:
+        print("Can't connect to database")
+        return 0
+    """If Connection Is Successful"""
+    print("Connected")
+        
+    cursor = db.cursor()
+
+    sql = "SELECT * FROM states ORDER BY states.id ASC"
+    """Create table as per requirement"""
+ 
+    cursor.execute(sql)
+
+    db.close()
+    """disconnect from server"""
+ 
+""" Function Call For Connecting To Our Database"""
+mysqlconnect()
