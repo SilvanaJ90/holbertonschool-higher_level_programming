113a671f9a7458c46d9f2170db23a9d4b14a8b9e
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-11-14 22:40:09 -0500

    update

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,5 +1,9 @@
 --  script to lists all genres of the show Dexter.
 SELECT A.name
 FROM tv_genres A
-WHERE id = (SELECT id FROM tv_shows WHERE title='Dexter')
+LEFT JOIN tv_show_genres B
+ON A.id = B.genre_id
+LEFT JOIN tv_shows C
+ON B.show_id = C.id
+WHERE title='Dexter'
 ORDER BY A.name ASC;
