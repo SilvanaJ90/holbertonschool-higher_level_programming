158a82a0f7461532b0127e8868e2f025b9cdae15
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-11-14 23:01:03 -0500

    update

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -2,8 +2,8 @@
 SELECT A.title
 FROM tv_shows A
 LEFT JOIN tv_show_genres B
-ON A.id = B._show_id
+ON A.id = B.show_id
 LEFT JOIN tv_genres C
-ON B.genre = C.id
+ON B.genre_id = C.id
 WHERE C.name = 'Comedy'
 ORDER BY A.title ASC;
