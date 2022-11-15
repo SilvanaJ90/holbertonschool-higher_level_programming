3ec8aa7341dd3850407be011bc00cbcda27b049b
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-11-14 21:39:51 -0500

    update

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -1,6 +1,8 @@
 -- Write a script that lists all shows contained in hbtn_0d_tvshows without a genre linked. 
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows 
-OUTER JOIN tv_show_genres
+FROM tv_show_genres
+RIGHT JOIN tv_shows
 ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_shows.id IS NULL
+OR tv_show_genres.show_id IS NULL
 ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
