ef0d95d61702f32839b9132fe37e36117ac8fa45
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-11-14 23:07:22 -0500

    update

---
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,8 @@
+-- script that lists all shows, and all genres.
+SELECT  A.title, C.name
+FROM tv_shows A
+LEFT JOIN tv_show_genres B
+ON A.id = B.show_id
+LEFT JOIN tv_genres C
+ON B.genre_id = C.id
+ORDER BY A.title, C.name ASC;
