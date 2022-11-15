cdddb8ac92f5a069b80c4de809db9a7e4b12a2eb
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-11-14 22:23:25 -0500

    update

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,7 @@
 -- script displays the number of shows linked to each.
 SELECT B.name AS genre, COUNT(A.show_id) AS number_of_shows
 FROM tv_show_genres A
-LEFT JOIN tv_genres B
+INNER JOIN tv_genres B
 ON A.genre_id = B.id
 GROUP BY A.genre_id
 ORDER BY number_of_shows DESC;
