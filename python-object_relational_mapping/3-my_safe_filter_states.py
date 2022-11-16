4e464b0afbf09520efb66ae8678f2fc54f182ae8
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-11-16 18:29:13 -0500

    update

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+Write a script that takes in an argument
+and displays all values in the states table of
+hbtn_0e_0_usa where name matches the argument.
+"""
+
+import MySQLdb
+from sys import argv
+
+
+def mysqlconnect():
+    """ Function for connecting to MySQL database"""
+    db = MySQLdb.connect(
+        host="localhost",
+        user=argv[1],
+        password='',
+        database=argv[3],
+        port=3306
+        )
+
+    cursor = db.cursor()
+
+    sql = "SELECT * FROM states WHERE name = BINARY '{}' \
+             ORDER BY states.id ASC".format(argv[4])
+
+    cursor.execute(sql)
+
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+
+    db.close()
+
+
+if __name__ == "__main__":
+    mysqlconnect()
