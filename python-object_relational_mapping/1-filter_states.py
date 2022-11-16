550c0de86041a43753d44051a435c48a6fc93035
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-11-16 16:22:56 -0500

    update

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" Write a script that lists all states from the database hbtn_0e_0_usa: """
+
+import MySQLdb
+from sys import argv
+
+
+def mysqlconnect():
+    """ Function for connecting to MySQL database"""
+    db = MySQLdb.connect(
+        host="localhost",
+        user=argv[1],
+        password='',
+        database=argv[3],
+        port=3306
+        )
+
+    cursor = db.cursor()
+
+    sql = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC;"
+
+    cursor.execute(sql)
+
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+
+    db.close()
+
+
+if __name__ == "__main__":
+    mysqlconnect()
