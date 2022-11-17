79cd1a59cddd4867873826637b69e49ea685bbda
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-11-17 16:03:31 -0500

    update

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -6,10 +6,12 @@ from sqlalchemy import Column, Integer, String
 
 Base = declarative_base()
 """ definition of a State and an instance Base = declarative_base(): """
+
+
 class State(Base):
     """ class state inherits from Base """
     __tablename__ = 'states'
     id = Column(Integer, autoincrement=True,
-             unique=True, nullable=False, 
-             primary_key=True)
+                unique=True, nullable=False,
+                primary_key=True)
     name = Column(String(128), nullable=False)
