a4355121ec148ff622b91ad017a9cb240f129400
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-11-17 12:22:51 -0500

    update

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """
-Write a script that takes in the name of
-a state as an argument and lists all cities
-of that state, using the database hbtn_0e_4_usa
+Write a script that takes in the name of a state as an argument
+and lists all cities of that state, using the database hbtn_0e_4_usa 
 """
 
 import MySQLdb
@@ -21,8 +20,8 @@ def mysqlconnect():
 
     cursor = db.cursor()
 
-    cursor.execute("SELECT * FROM states WHERE name = BINARY '{}' \
-             ORDER BY states.id ASC".format(argv[4]))
+    cursor.execute("SELECT * FROM states WHERE name = %s \
+            ORDER BY states.id ASC", (argv[4], ))
 
     rows = cursor.fetchall()
     for col in rows:
