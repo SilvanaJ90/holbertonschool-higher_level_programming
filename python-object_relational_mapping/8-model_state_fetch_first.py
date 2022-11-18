69227a4b18469cd719b96c74be82cb427b755495
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-11-18 14:07:40 -0500

    create1

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,21 +1,27 @@
 #!/usr/bin/python3
-""" Start link class to table in database
+"""
+Start link class to table in database
 """
 import sys
 from model_state import Base, State
-
-from sqlalchemy import (create_engine)
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine, MetaData
+from sqlalchemy.orm import Session
 
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
                            format(sys.argv[1], sys.argv[2],
                                   sys.argv[3]), pool_pre_ping=True)
+
+    engine.connect()
+
+    metaData = MetaData()
+
+    session = Session(engine)
+
     Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    states = session.query(State).all()
-    for state in states:
+
+    for state in session.query(State).limit(1).all():
         print("{}: {}".format(state.id, state.name))
+
     session.close()
