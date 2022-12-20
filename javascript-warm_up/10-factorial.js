7d15dbaa615efe59c67c3e05a1a8257f360f8eb6
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-12-20 14:54:53 -0500

    update

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
+const num = parseInt(process.argv[2])
+
 function factorialize (num) {
-  if (num === 0 || num === 1 || isNaN(num))
+  if (num === 1 || isNaN(num)) {
     return 1;
-  for (let i = num - 1; i >= 1; i--) {
-    num *= i;
   }
-  return num;
+  return num * factorialize(num - 1);
 }
-console.log(factorialize(parseInt(process.argv[2])));
+console.log(factorialize(num));
