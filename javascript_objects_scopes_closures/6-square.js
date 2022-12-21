c16a18ac3bde2c81596a1d69d57fe02281c8f4c7
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-12-21 17:09:26 -0500

    update

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -5,7 +5,7 @@ const Sqr = require('./5-square');
 module.exports = class Square extends Sqr {
   charPrint (c) {
     if (!c) {
-      (c = 'X')
+      (c = 'X');
     }
     for (let i = 0; i < this.height; i++) {
       console.log(c.repeat(this.width));
