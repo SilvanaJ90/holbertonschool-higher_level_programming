7a7536462387848ded952f4169d08a5df8a1a299
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-12-21 17:08:13 -0500

    update

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -4,14 +4,11 @@ const Sqr = require('./5-square');
 
 module.exports = class Square extends Sqr {
   charPrint(c) {
-    if (c) {
-      for (let i = 0; i < this.height; i++) {
-        console.log(c.repeat(this.width));
-          } 
-    } else {
-        for (let i = 0; i < this.height; i++) {
-          console.log((c = 'X').repeat(this.width));
-        }
+    if (!c) {
+      (c = 'X')
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
     }           
   }
 };
