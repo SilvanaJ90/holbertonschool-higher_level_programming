97077d9cc1cc6b11408a9d8b5cb33dace01dee7c
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-12-21 17:31:17 -0500

    create-count

--- javascript_objects_scopes_closures/7-occurrences.js
+++ javascript_objects_scopes_closures/7-occurrences.js
@@ -1,8 +1,11 @@
 #!/usr/bin/node
 
 exports.nbOccurences = function (list, searchElement) {
-
-  list.reduce((acc, elem) => {
-    return (searchElement === elem ? acc + 1 : acc)
-  });
-};
+  let acc = 0;
+  for (let elem = 0; elem < list.length; elem++) {
+    if (list[elem] === searchElement) {
+      acc++
+    }
+  }
+  return acc;
+}
