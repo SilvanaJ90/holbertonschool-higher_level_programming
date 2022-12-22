e88ea37c6e6008284a76def2b163590b88a2d325
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-12-22 14:53:21 -0500

    update

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -3,10 +3,10 @@
 const request = require('request');
 url_sw = 'https://swapi-api.hbtn.io/api/films/:id';
 
-request.get(url_sw, process.argv[2], 
+request.get(url_sw + process.argv[2], 
   function (err, response, body) {
     if (err) {
-      console.log(err)
+      return;
     }
     console.log(JSON.parse(body).title);
 });
