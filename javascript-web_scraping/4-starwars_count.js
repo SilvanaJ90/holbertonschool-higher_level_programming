fcf3ad42b25eb100e34ec85433ca53ad68ce9675
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-12-22 17:31:10 -0500

    update

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 
 const request = require('request');
-const url = 'https://swapi-api.hbtn.io/api/people/18';
-const moveId = process.argv[2];
+const url = process.argv[2];
+const moveId = 'https://swapi-api.hbtn.io/api/people/18';
 
-request.get(url + moveId, function (err, res, body) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(JSON.parse(body));
+request.get(url, function (err, res, body) {
+  if (moveId) {
+    count++
+  return count
   }
 });
+console.log(count);
