e42e085c808c75c49b1fcf03745790a37ff5756c
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-12-22 14:20:27 -0500

    update

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -4,9 +4,9 @@ const request = require('request');
 url_sw = 'https://swapi-api.hbtn.io/api/films/:id';
 
 request.get(url_sw, process.argv[2], 
-  function (err, response) {
+  function (err, response, body) {
   if (err) {
     return;
   }
-  console.log(JSON.parse(response).title);
+  console.log(JSON.parse(body).title);
 });
