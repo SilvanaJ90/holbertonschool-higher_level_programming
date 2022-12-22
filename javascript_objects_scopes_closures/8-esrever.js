9e62c95a3b0f71f6cabd61c46391978e7ada738f
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-12-21 20:16:55 -0500

    update

--- javascript_objects_scopes_closures/8-esrever.js
+++ javascript_objects_scopes_closures/8-esrever.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
 
 exports.esrever = function (list) {
-  const new_list = [];
-  for (let i = list.length - 1; i >= 0; i--) {
-    new_list.push(list[i]);
- }
- return new_list;
-};
+    const newList = [];
+    for (let i = list.length - 1; i >= 0; i--) {
+      newList.push(list[i]);
+    }
+    return newList;
+  };
+  
