121376c008c64eba18ac7e44d2af1e2fe1dcdb09
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-12-22 20:54:54 -0500

    create-task5

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -5,7 +5,7 @@ const request = require('request');
 const url = process.argv[2];
 const filename = process.argv[3];
 
-request.get(url, function (err, res, body) {
+request.get(url, function (res, body) {
   fs.writeFile(filename, body, 'utf-8', function (err) {
     if (err) {
       console.log(err);
